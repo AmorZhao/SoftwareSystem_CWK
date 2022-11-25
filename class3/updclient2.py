3a97c22512cab7b514a5eea23a05e5f81f76a8f0
AmorZhao <amor_7303@163.com>
2022-11-25 16:31:32 +0000

    first commit

---
+++ class3/updclient2.py
@@ -0,0 +1,21 @@
+import socket
+import time
+#the server name and port client wishes to access
+server_name = 'localhost'
+server_port = 12000
+client_port2 = 13000
+#create a UDP client socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.bind (('', client_port2))
+
+print("UDP client running...");
+print("Connecting to server at IP: ", server_name, " PORT: ", server_port)
+
+for i in range (1, 101):
+    msg = str(i)
+    time.sleep(1)
+    
+    #send the message to the udp server
+    client_socket.sendto(msg.encode(),(server_name, server_port))
+
+#client_socket.close()
